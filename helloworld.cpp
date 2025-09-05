57c2f076853874459d76504e036c2595eea83989
Jude <judethedude0512@outlook.com>
2025-09-05 10:33:10 -0400

    ummmmmmm, c++ ig

---
+++ helloworld.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int one = 5;
+    cout << one << endl;
+    double two = 6.741;
+    cout << two << endl;
+    char three = 'H';
+    cout << three << endl;
+    string four = "Mangos";
+    cout << four << endl;
+    bool five = true;
+    cout << five << endl;
+}
